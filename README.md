3ca6e052152ec64299aed21dc50914e77baf9c02
sauliuspr <saulius.prasmantas@yarowa.com>
2023-12-15 11:58:26 +0100

    Inital commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Yarowa City-status
 
 ## Getting Started
 
@@ -20,15 +20,11 @@ This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-opti
 
 ## Learn More
 
+To learn more about [Yarowa Cloud Amdin Documentation](https://jarowa.atlassian.net/wiki/spaces/_/pages/2740289537?atlOrigin=eyJpIjoiMzQyMjJjMTcyNmMyNGZmN2E2NDJiZDM5OGNmMTY1NmYiLCJwIjoiYyJ9)
+
 To learn more about Next.js, take a look at the following resources:
 
 - [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
 - [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
 You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,66 @@
-import Image from 'next/image'
+import React from 'react'
+import { NetworkError, handleNetworkError } from './utils/networkHelpers';
+import CityCards from "./components/CityCards"
 
-export default function Home() {
+async function getBusinessUnits<BusinessUnit>(url: string): Promise<BusinessUnit[]> {
+  const res = await fetch(url);
+  if (!res.ok) {
+    await handleNetworkError(res, url);
+  }
+  const data = (await res.json()).data as BusinessUnit[];
+  return data;
+}
+
+async function getCities<City>(url: string): Promise<City[]> {
+  const res = await fetch(url);
+  if (!res.ok) {
+    await handleNetworkError(res, url);
+  }
+  const data = (await res.json()).data as City[];
+  return data;
+}
+
+async function fetchCityOnlineDiagnostics(url: string): Promise<CityDetails>  {
+  const res = await fetch(url);
+  if (!res.ok) {
+    await handleNetworkError(res, url);
+  }
+  const data = (await res.json()) as CityDetails;
+  return data;
+}
+
+const CitiesList = async() => {
+  const apiURL = process.env.JAROWA_CITY_API_URL || 'https://app-clouds-admin-pool.azurewebsites.net';
+  const botApiURL = process.env.JAROWA_CITY_BOT_API_URL || 'https://app-clouds-bot-api-pool.azurewebsites.net';
+  const apiKey = process.env.JAROWA_CITY_API_KEY;
+
+  const units: BusinessUnit[] = await getBusinessUnits(`${apiURL}/api/businessUnits?code=${apiKey}`);
+  var cities: City[] = []
+  for (let unit of units ) {
+     const newCities: City[] = await getCities(`${apiURL}/api/cities?code=${apiKey}&businessUnit=${unit.businessUnit}`)
+     if (newCities && newCities.length > 0){
+       cities = cities.concat(newCities);
+      }
+  }
+  const brokenCities: City[] =[]
+  for (let city of cities) {
+    const diagnostics = await fetchCityOnlineDiagnostics(`${botApiURL}/Admin/City/${city.key}/onlineDiagnostics?wait=false&refresh=false`);
+      if (diagnostics.regions){
+        city.onlineDiagnostics = diagnostics;
+        if (diagnostics.regions[0]?.frontend.error || diagnostics?.regions[0]?.backend.error ) {
+          const lastDiagnostics = await fetchCityOnlineDiagnostics(`${botApiURL}/Admin/City/${city.key}/lastEnableDiagnostics`);
+          city.lastDiagnostics = lastDiagnostics
+        }
+      } else {
+        brokenCities.append(city.key)
+      }
+  }
+    
   return (
-    <main>
-      <h1>Cities Status</h1>
-    </main>
+    <>
+    <CityCards cities={cities}/>
+    </>
   )
 }
+
+export default CitiesList
